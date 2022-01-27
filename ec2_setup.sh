2150e8be5cee19c040512defc62cb763a3a22349
J. Ryan Shue <ryan_shue23@milton.edu>
2022-01-27 18:48:34 -0500

    Update ec2_setup.sh

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -3,3 +3,8 @@
 wget https://repo.continuum.io/archive/Anaconda3-5.3.1-Linux-x86_64.sh
 bash Anaconda3-5.3.1-Linux-x86_64.sh
 export PATH="/home/ubuntu/anaconda3/bin:$PATH"
+
+# Clone and build ldif
+git clone https://github.com/JRyanShue/ldif.git
+cd ldif
+conda env create --name ldif -f environment.yml
