87054c6cef0fe4057f9c9593030ca741cbc4e51b
JRyanShue <ryanshue.design@gmail.com>
2022-06-10 00:29:34 +0000

    Add Satori training scripts

--- run_training.sh
+++ run_training.sh
@@ -1,7 +1,15 @@
 #!bin/bash
 conda env create --name ldif -f environment.yml
-conda activate ldif
+source activate ldif
 # download_{tiny_subsample, shapenet_chair, full_shapenet}.sh
-bash scripts/tiny_subsample.sh
+bash scripts/download_tiny_subsample.sh
 # python setup.py build_ext --inplace  # Not needed if only using IoU for metrics
-python train.py --batch_size 24 --dataset_directory /mnt/c/users/jryan/Documents/GitHub/ldif/watertight_ds --experiment_name ldif --model_type ldif
+
+# For training, rename dir
+mv ../shapenet/shapenet_ldif/val ../shapenet/shapenet_ldif/train
+
+# Download &unzip checkpoint
+wget https://imt-public-datasets.s3.amazonaws.com/pretrained_models/ldif_48shape.zip && unzip ldif_48shape.zip
+
+# Checkpoint uploads to s3
+python train.py --batch_size 24 --dataset_directory ../shapenet/shapenet_ldif --experiment_name ldif --model_type ldif --checkpoint_interval 1000
--- train.py
+++ train.py
@@ -42,6 +42,8 @@ from ldif.util import file_util
 from ldif.util import gpu_util
 from ldif.util import path_util
 from ldif.util.file_util import log
+
+import subprocess
 # pylint: enable=g-bad-import-order
 # pylint: enable=g-import-not-at-top
 
@@ -271,6 +273,14 @@ def main(argv):
         ckpt_path = os.path.join(checkpoint_dir, 'model.ckpt')
         log.info(f'Writing checkpoint to {ckpt_path}...')
         saver.save(session, ckpt_path, global_step=i)
+
+        # zip and upload to s3
+        log.info(f'Zipping model checkpoints...')  # No compression
+        subprocess.run(['rm', 'ldif_48shape.zip'])
+        subprocess.run(['zip', '-r', '-0', 'ldif_48shape.zip', 'ldif/trained_models'])
+        log.info(f'Uploading model checkpoints to S3...')
+        subprocess.run(['aws', 's3', 'cp', 'ldif_48shape.zip', 's3://imt-public-datasets/pretrained_models/'])
+    
     log.info('Done training!')
 
 
